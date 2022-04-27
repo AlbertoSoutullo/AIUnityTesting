e886ed8ae78c54a79c7dfe58bda0f1d47dd9170f
Alberto <alberto.soutullo@hotmail.com>
2022-04-27 02:00:20 +0200

    Preparing classes to move player and prefabs instantiation

---
+++ Assets/Scripts/PCG/PutPlayerInMap.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace PCG
+{
+    public class PutPlayerInMap : MonoBehaviour
+    {
+        public static GameObject Player;
+        public static GameObject Hunter;
+        
+        public static LayerMask TerrainLayer;
+        
+        private void Start()
+        {
+            float positionYPlayer = 9999;
+            float positionYHunter = 9999;
+            RaycastHit hit;
+            
+            if (Physics.Raycast(new Vector3(0, 9999f, 0), Vector3.down,
+                    out hit, Mathf.Infinity, TerrainLayer))
+            {
+                positionYPlayer = hit.point.y;
+                positionYHunter = hit.point.y;
+            }
+
+            positionYPlayer += 1.5f / 2;
+            positionYHunter += Hunter.GetComponent<CapsuleCollider>().height / 2;
+            Vector3 positionPlayer = new Vector3(0, positionYPlayer, 0);
+            Vector3 positionHunter = new Vector3(3, positionYHunter, 0);
+
+            var playerObject = Instantiate(Player, positionPlayer, Quaternion.identity);
+            Instantiate(Hunter, positionHunter, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/PCG/PutPrefabsInMap.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+using PCG.Data;
+
+namespace PCG
+{
+    public class PutPrefabsInMap
+    {
+        //todo
+        /*
+        private void GeneratePrefabs(MeshData mesh)
+        {
+            // loop through all triangles and select the positions to where instantiate prefabs
+            int[] triangles = mesh.triangles;
+            Vector3[] positions = new Vector3[triangles.Length / 3];
+            int count = 0;
+            for (int i = 0; i < triangles.Length; i += 3)
+            {
+                // only selecting the first vertex for each triangle (greedy approach)
+                positions[count] = mesh.vertices[triangles[i]];
+                ++count;
+            }
+        
+            // determine where to instantiate the prefabs
+            PrefabsInternalData prefabsInternalData = PrefabsGenerator.DeterminePrefabsPositions(ChunkSize, 
+                ChunkSize, terrainData.meshHeightMultiplier, positions, prefabsData, normalizeMode, useFalloff,
+                fallOffMap);
+        
+            // instantiate the prefabs
+            MapDisplay display = FindObjectOfType<MapDisplay> ();
+            display.InstantiatePrefabs(prefabsInternalData);
+        }
+        */
+    }
+}
