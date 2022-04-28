17b8346d2a4459560b67ac69a797d100bb8cf5d2
AlbertoSoutullo <alberto.soutullo@hotmail.com>
2022-04-28 13:11:28 +0200

    Added script to delete preview map when entering in play mode

---
+++ Assets/Scripts/PCG/HideOnPlay.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PCG
+{
+    public class HideOnPlay : MonoBehaviour {
+        
+        void Start () {
+            gameObject.SetActive (false);
+        }
+
+    }
+}
