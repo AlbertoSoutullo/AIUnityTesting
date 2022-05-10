be3507421d04876dd8f7cac9c39eaa43e91186e1
Alberto <alberto.soutullo@hotmail.com>
2022-05-10 21:23:41 +0200

    Hunter now correctly points to arrow to pick up

--- Assets/HunterAI/Scripts/PointState.cs
+++ Assets/HunterAI/Scripts/PointState.cs
@@ -6,15 +6,16 @@ namespace HunterAI.Scripts
     {
         public static PointState instance { get; } = new PointState();
 
+        private GameObject _arrow;
         static PointState() {}
 
         public override void Enter(CompanionMovement companion)
         {
             // Debug.Log("Entering PointState");
-            GameObject arrow = companion.GetClosestArrow();
-            if (arrow != null)
+            _arrow = companion.GetClosestArrow();
+            if (_arrow != null)
             {
-                companion.transform.LookAt(arrow.transform.position);
+                companion.transform.LookAt(_arrow.transform.position);
                 companion.Point();
             }
         }
@@ -22,6 +23,10 @@ namespace HunterAI.Scripts
         public override void Execute(CompanionMovement companion)
         {
             Debug.Log("Executing in point state");
+            if (_arrow != null)
+            {
+                companion.transform.LookAt(_arrow.transform.position);
+            }
 
             if (companion.arrows > 0)
                 companion.GetFsm().ChangeState(IdleState.instance);
