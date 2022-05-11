8d65167e95dcb43b3a6d527b517c41ca44b27ee8
AlbertoSoutullo <alberto.soutullo@hotmail.com>
2022-05-11 12:31:40 +0200

    Finished hunter refactor. Pending delete unused functions

--- Assets/HunterAI/Scripts/Companion.cs
+++ Assets/HunterAI/Scripts/Companion.cs
@@ -1,10 +1,6 @@
 // Unity Imports
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 // Project Imports
 using CustomFSM;
@@ -64,6 +60,7 @@ namespace HunterAI.Scripts
 			return _stateMachine;
 		}
 		
+		/* todo delete?
 		IEnumerator OnCompleteAttackAnimation()
 		{
 			yield return new WaitUntil(() => _animationController.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f);
@@ -74,7 +71,7 @@ namespace HunterAI.Scripts
 		{
 			return _animationController.GetCurrentAnimatorStateInfo(0).IsName("Shoot")
 			       && _animationController.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f;
-		}
+		}*/
 
 		public void Attack()
 		{
@@ -87,48 +84,6 @@ namespace HunterAI.Scripts
 			_animationController.SetBool(NoAmmo, true);
 		}
 		
-		public void FinishPointing()
-		{
-			_animationController.SetBool(NoAmmo, false);
-			Debug.Log("Going back to IdleState");
-			GetFsm().ChangeState(IdleState.instance);
-		}
-
-		public void FinishAttack()
-		{
-			_animationController.SetBool(Shoot, false);
-
-			if (arrows <= 0)
-			{
-				Debug.Log("No arrows left so changing to PointState");
-				GetFsm().ChangeState(PointState.instance);
-			}
-			else
-			{
-				GameObject currentClosestEnemy = GetClosestEnemy();
-				if (currentClosestEnemy == null)
-				{
-					Debug.Log("No enemies can be attacked anymore so changing to IdleState");
-					GetFsm().ChangeState(IdleState.instance);
-				}
-				else if (currentClosestEnemy != _currentTarget)
-				{
-					Debug.Log("Current target is no longer the closest so changing to ChooseTargetState");
-					GetFsm().ChangeState(ChooseTargetState.instance);
-				}
-				else
-				{
-					if (Vector3.Distance(transform.position, _currentTarget.transform.position) > weaponRangeDistance)
-					{
-						Debug.Log("No longer in current target's range so changing to RelocateState");
-						GetFsm().ChangeState(RelocateState.instance);
-					}
-					else
-						GetFsm().ChangeState(AttackState.instance);
-				}
-			}
-		}
-
 		public CompanionMovement GetMovement()
 		{
 			return gameObject.GetComponent<CompanionMovement>();
--- Assets/HunterAI/Scripts/CompanionAnimationEvents.cs
+++ Assets/HunterAI/Scripts/CompanionAnimationEvents.cs
@@ -1,9 +1,66 @@
-﻿using UnityEngine;
+﻿using HunterAI.Scripts.FSMStates;
+using UnityEngine;
 
 namespace HunterAI.Scripts
 {
     public class CompanionAnimationEvents: MonoBehaviour
     {
+        private Animator _animationController;
 
+        private Companion _companion;
+        private CompanionLogic _logic;
+        
+        private static readonly int Shoot = Animator.StringToHash("shoot");
+        private static readonly int NoAmmo = Animator.StringToHash("noAmmo");
+        
+        private void Start()
+        {
+            _animationController = GetComponent<Animator>();
+            _companion = GetComponent<Companion>();
+            _logic = GetComponent<CompanionLogic>();
+        }
+
+        public void FinishPointing()
+        {
+            _animationController.SetBool(NoAmmo, false);
+            Debug.Log("Going back to IdleState");
+            _companion.GetFsm().ChangeState(IdleState.instance);
+        }
+        
+        public void FinishAttack()
+        {
+            _animationController.SetBool(Shoot, false);
+
+            if (_companion.arrows <= 0)
+            {
+                Debug.Log("No arrows left so changing to PointState");
+                _companion.GetFsm().ChangeState(PointState.instance);
+            }
+            else
+            {
+                GameObject currentClosestEnemy = _logic.GetClosestEnemy();
+                if (currentClosestEnemy == null)
+                {
+                    Debug.Log("No enemies can be attacked anymore so changing to IdleState");
+                    _companion.GetFsm().ChangeState(IdleState.instance);
+                }
+                else if (currentClosestEnemy != _companion.GetCurrentTarget())
+                {
+                    Debug.Log("Current target is no longer the closest so changing to ChooseTargetState");
+                    _companion.GetFsm().ChangeState(ChooseTargetState.instance);
+                }
+                else
+                {
+                    if (Vector3.Distance(transform.position, _companion.GetCurrentTarget().transform.position) > 
+                        _logic.weaponRangeDistance)
+                    {
+                        Debug.Log("No longer in current target's range so changing to RelocateState");
+                        _companion.GetFsm().ChangeState(RelocateState.instance);
+                    }
+                    else
+                        _companion.GetFsm().ChangeState(AttackState.instance);
+                }
+            }
+        }
     }
 }
